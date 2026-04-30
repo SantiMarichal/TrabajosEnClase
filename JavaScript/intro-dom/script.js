2ea136509b207b9fe83bce2cda1e2e7d31bf5f18
SantiMarichal <marichalutu@gmail.com>
2026-04-30 11:33:00 -0300

    JavaScript

---
+++ JavaScript/intro-dom/script.js
@@ -0,0 +1,12 @@
+console.log("Hola Mundo");
+
+let saludo = "Hola mundo let";
+let num = 67;
+let num_coma = 6.9;
+let existe = true;
+
+console.log("Variable Saludo" + ": " + saludo + " " + (typeof saludo));
+console.log("Variable Num" + ": " + num + " " + (typeof num));
+console.log("Variable Num Coma" + ": " + num_coma + " " + (typeof num_coma));
+console.log("Variable Existente" + ": " + existe + " " + (typeof existe));
+/* typeof es una funcion que nos indica el tipo de dato de la variable en ese momento de ejecucion */
