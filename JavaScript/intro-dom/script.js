8054b313a72e88145260423c1a2d07ecddd558d5
SantiMarichal <marichalutu@gmail.com>
2026-04-30 12:13:53 -0300

    JavaScript

--- JavaScript/intro-dom/script.js
+++ JavaScript/intro-dom/script.js
@@ -9,4 +9,17 @@ console.log("Variable Saludo" + ": " + saludo + " " + (typeof saludo));
 console.log("Variable Num" + ": " + num + " " + (typeof num));
 console.log("Variable Num Coma" + ": " + num_coma + " " + (typeof num_coma));
 console.log("Variable Existente" + ": " + existe + " " + (typeof existe));
+
 /* typeof es una funcion que nos indica el tipo de dato de la variable en ese momento de ejecucion */
+
+const parrafo = document.getElementById("txt");
+
+parrafo.textContent = "Cambiando texto desde JavaScript";
+
+const boton = document.getElementById("btn");
+boton.addEventListener("click", () => (
+    parrafo.textContent="Cambiando texto con un botòn en JS"
+));
+
+document.getElementById("btn-error").addEventListener("click" , () => {alert("Error 666")
+});
