c25272e010979097cca162063a8113d9adabfe6c
Valentina Garcia <valuchygarcia@gmail.com>
2026-05-28 12:51:53 -0300

    agregar

--- JavaScript/event/filtrado/main.js
+++ JavaScript/event/filtrado/main.js
@@ -17,11 +17,46 @@ const pokedex = [
     }
 ];
 
+//Seleccionamos todos los button que estan dentro del contenedor id `filtros`
 const botonesFiltro = document.querySelectorAll(`#filtros button`);
-
+//Selecciono la coleccion de divs que tienen la clase pokemon card
+const cards = document.querySelectorAll(`.pokemon-card`);
 
 botonesFiltro.forEach((btn) => {
     btn.addEventListener(`click` , (e) => {
-        console.log(btn.database.tipo);
+        console.log(btn.dataset.tipo);
+
+        const tipoSeleccionado = btn.dataset.tipo;
+
+        //Recorremos la coleccion de las cards
+        cards.forEach((card) => {
+            if(tipoSeleccionado==card.dataset.tipo || tipoSeleccionado==`todos`){
+                //la card que quiero mostrar
+                card.style.display=`flex`;
+            }else{
+                card.style.display=`none`;
+            };
+        });
+    });
+});
+
+const inputFiltro = document.querySelector(`#txtFiltro`);
+
+inputFiltro.addEventListener(`input` , () =>{
+    const textoFiltro=inputFiltro.value.toLowerCase();
+    cards.forEach((card) => {
+        if (card.dataset.tipo.includes(textoFiltro) || card.dataset.tipo.includes(`todos`)){
+            card.style.display = `flex`;
+        }else{
+            card.style.display=`none`;
+        }
+
+
+        if(textoFiltro==card.dataset.tipo || textoFiltro==`todos`){
+            //la card que quiero mostrar
+            card.style.display=`flex`;
+        }else{
+            card.style.display=`none`;
+        };
     });
 });
