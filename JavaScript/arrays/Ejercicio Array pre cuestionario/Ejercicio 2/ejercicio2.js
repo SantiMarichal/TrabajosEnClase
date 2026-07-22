90eace8e910ee2ff9f5406f0b37bb4b0156faf4e
SantiMarichal <marichalutu@gmail.com>
2026-07-22 15:50:49 -0300

    fix: ejercicio2.js

--- JavaScript/arrays/Ejercicio Array pre cuestionario/Ejercicio 2/ejercicio2.js
+++ JavaScript/arrays/Ejercicio Array pre cuestionario/Ejercicio 2/ejercicio2.js
@@ -1,6 +1,6 @@
 const tags = ['javascript', 'web', 'programación', 'frontend', 'react'];
 // 1) Comprobar que la etiqueta 'python' está presente en el array.
-alert("python esta incluido: " +tags.includes("python"));
+alert("python esta incluido: " + tags.includes("python"));
 // 2) Crear un nuevo array llamado 'principales' que contenga solo las 3 primeras etiquetas.
 const principales = tags.slice(0 , 3);
 alert("El nuevo array es: " + principales);
